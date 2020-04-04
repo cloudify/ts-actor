0c3552f39ec2e3b9349d01e4bd2b81945d4fec87
Federico Feroldi <pix@yahoo.it>
2020-04-04 21:49:15 +0200

    Sender should be part of message

--- index.ts
+++ index.ts
@@ -1,36 +1,56 @@
-abstract class Actor {
-  private inbox: { message: unknown; sender: Actor }[] = [];
+//
+// ACTOR SYSTEM
+//
 
-  public push(message: unknown, sender: Actor): void {
-    this.inbox.push({ message, sender });
+abstract class Actor {
+  private inbox: unknown[] = [];
+
+  public push(message: unknown): void {
+    this.inbox.push(message);
     process.nextTick(() => this.processOne());
   }
 
   private processOne(): void {
-    const head = this.inbox.shift();
-    if (head !== undefined) {
-      const { message, sender } = head;
-      this.receive(message, sender);
+    const message = this.inbox.shift();
+    if (message !== undefined) {
+      this.onReceive(message);
     }
   }
 
-  public abstract receive(message: unknown, sender: Actor): void;
+  public abstract onReceive(message: unknown): void;
 }
 
+//
+// EXAMPLE ACTORS
+//
+
+import * as t from "io-ts";
+import { either } from "fp-ts/lib/Either";
+
+const Message = t.type({
+  sender: t.any,
+});
+
 class Actor1 extends Actor {
-  public receive(message: unknown, sender: Actor): void {
-    console.log(message);
-    sender.push("PING", this);
+  public onReceive(message: unknown): void {
+    either.map(Message.decode(message), ({ sender }) => {
+      console.log("A1:PING");
+      sender.push(Message.encode({ sender: this }));
+    });
   }
 }
 
 class Actor2 extends Actor {
-  public receive(message: unknown, sender: Actor): void {
-    console.log(message);
-    sender.push("PONG", this);
+  public onReceive(message: unknown): void {
+    either.map(Message.decode(message), ({ sender }) => {
+      console.log("A2:PONG");
+      sender.push(Message.encode({ sender: this }));
+    });
   }
 }
 
 const a1 = new Actor1();
 const a2 = new Actor2();
-a1.push("PING", a2);
+
+// send Message to a1, from a2
+a1.push(Message.encode({ sender: a2 }));
